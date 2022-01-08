dd2a62d9eb7869c132cf2e5f3da94d5226e5f05b
Garance CLAUSSE <gclausse@e1r11p20.42.fr>
2022-01-08 20:16:32 +0100

    some issues resolved

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: gclausse <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/08 15:32:53 by gclausse          #+#    #+#             */
-/*   Updated: 2022/01/08 20:00:47 by gclausse         ###   ########.fr       */
+/*   Updated: 2022/01/08 20:16:14 by gclausse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,7 @@ char	*parse_path(char *path, char *cmd)
 	return (NULL);
 }
 
-void	cmd1(char **argv, char **env)
+void	cmd1(char **argv, char **env, int *fd)
 {
 	int		file;
 	char	*path;
@@ -59,24 +59,26 @@ void	cmd1(char **argv, char **env)
 	if (file == -1)
 		exit (errno);
 	dup2(file, STDIN_FILENO);
+	dup2(fd[1], STDOUT_FILENO);
 	close(file);
 	execve(path, cmd1, env);
 }
 
-void	cmd2(char **argv, char **env)
+void	cmd2(char **argv, char **env, int *fd)
 {
 	int		file2;
 	char	*path2;
 	char	**cmd2;
 
+	close(fd[1]);
 	cmd2 = ft_split(argv[3], ' ');
 	path2 = parse_path(get_path(env), cmd2[0]);
-	file2 = open(STDOUT_FILENO, O_RDONLY, 0777);
+	file2 = open(argv[4], O_WRONLY | O_CREAT, 0777);
 	if (file2 == -1)
 		exit (errno);
+	dup2(file2, STDOUT_FILENO);
 	dup2(fd[0], STDIN_FILENO);
 	close(fd[0]);
-	dup2(file2, STDOUT_FILENO);
 	close(file2);
 	execve(path2, cmd2, env);
 }
@@ -85,6 +87,8 @@ int	main(int argc, char **argv, char **env)
 {
 	pid_t	pid1;
 	pid_t	pid2;
+	int		wstatus;
+	int		fd[2];
 
 	if (argc == 5)
 	{
@@ -93,18 +97,19 @@ int	main(int argc, char **argv, char **env)
 		pid1 = fork();
 		if (pid1 < 0)
 			return (2);
+		wstatus = 0;
 		if (pid1 == 0) //child process
-			cmd1(argv, env);
+			cmd1(argv, env, fd);
 		if (pid1 > 0) //parent process
 		{
 			close(fd[1]);
 			pid2 = fork();
 			if (pid2 == 0) // new child
-				cmd2(argv, env);
+				cmd2(argv, env, fd);
 			else // still parent
 			{
-				waitpid(pid1, &wstatus, NULL);
-				waitpid(pid2, &wstatus, NULL);
+				waitpid(pid1, &wstatus, 0);
+				waitpid(pid2, &wstatus, 0);
 				close(fd[1]);
 				close(fd[0]);
 			}
