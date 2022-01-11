efd692de36b32858dd726f9610d1a0852a589ced
Garance CLAUSSE <gclausse@e1r5p14.42.fr>
2022-01-11 12:57:49 +0100

    libft added added

---
+++ libft/ft_putnbr_base.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_base.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gclausse <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/02 13:47:55 by gclausse          #+#    #+#             */
+/*   Updated: 2022/01/10 14:36:28 by gclausse         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_putnbr_base(int nbr, char *base)
+{
+	int			j;
+	long int	nb2;
+	int			cpt;
+
+	j = 0;
+	nb2 = nbr;
+	cpt = 1;
+	if (nb2 < 0)
+	{
+		ft_putchar_fd('-', 1);
+		nb2 = nb2 * -1;
+		cpt++;
+	}
+	if (nb2 < (long)ft_strlen(base))
+		ft_putchar_fd(base[nb2], 1);
+	else
+	{
+		cpt += ft_putnbr_base(nb2 / ft_strlen(base), base);
+		ft_putchar_fd(base[nb2 % ft_strlen(base)], 1);
+	}
+	return (cpt);
+}
---
+++ libft/ft_putnbr_usgn_lg.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_usgn_lg.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gclausse <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/03 19:50:10 by gclausse          #+#    #+#             */
+/*   Updated: 2022/01/10 14:36:55 by gclausse         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	putul(unsigned long int nbr, char *base)
+{
+	int					j;
+	unsigned long int	nb2;
+	int					cpt;
+
+	j = 0;
+	cpt = 1;
+	if (nbr < 0)
+		nb2 = nbr + 2147483648;
+	else
+		nb2 = (unsigned long int)nbr;
+	if (nb2 < (unsigned long int)ft_strlen(base))
+		ft_putchar_fd(base[nb2], 1);
+	else
+	{
+		cpt += putul(nb2 / ft_strlen(base), base);
+		ft_putchar_fd(base[nb2 % ft_strlen(base)], 1);
+	}
+	if (nbr < 0)
+		cpt -= 2;
+	return (cpt);
+}
